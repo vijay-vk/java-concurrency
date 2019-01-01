d1aa16cd7d39dd377db72ecd6795f11ae6806655
Kasiviswanathan, Vijay <vkasiviswanathan@paypal.com>
2019-01-01 18:26:19 +0530

    Created new project to test java-concurrency-frameworks

---
+++ src/main/java/org/learning/concurrency/javaconcurrency/JavaConcurrencyApplication.java
@@ -0,0 +1,14 @@
+package org.learning.concurrency.javaconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyApplication.class, args);
+	}
+
+}
+
